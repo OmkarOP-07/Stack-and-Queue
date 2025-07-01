bbe0c315b51e85015587f4ea18b493fee3ba4391
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-07-01 22:42:08 +0530

    made changes in code

---
+++ Intro/QueueExample.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+    st.push(5);
+    st.push(4);
+    st.push(3);
+    st.push(2);
+    st.push(1);
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top()<<endl;
+    st.pop();
+ return 0;
+}
