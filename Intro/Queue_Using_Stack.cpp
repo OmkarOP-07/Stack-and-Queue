2e06dd39461469566328667e7891e58516e7ae7f
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-07-19 20:29:51 +0530

    Queue using stack

--- Intro/Queue_Using_Stack.cpp
+++ Intro/Queue_Using_Stack.cpp
@@ -10,13 +10,13 @@ class Queue{
     public:
     
     void push(int x){
-        for (int i = 0; i < s1.size(); i++)
+        while(!s1.empty())
         {
             s2.push(s1.top());
             s1.pop();
         }
         s1.push(x);
-        for (int i = 0; i < s2.size(); i++)
+        while(!s2.empty())
         {
             s1.push(s2.top());
             s2.pop();
@@ -24,7 +24,7 @@ class Queue{
     }
 
     void pop(){
-     s1.pop();
+        s1.pop();
     }
     int top(){
         return s1.top();
@@ -38,13 +38,9 @@ int main()
 {
     Queue q1;
     q1.push(3);
-    cout<<q1.top()<<endl;
     q1.push(4);
-    cout<<q1.top()<<endl;
     q1.push(5);
-    cout<<q1.top()<<endl;
-
-    q1.pop();
+    
     cout<<q1.top()<<endl;
     q1.pop();
     cout<<q1.top()<<endl;
