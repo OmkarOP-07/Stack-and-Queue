86ccd86ea420345216f2982830e7cdf5de4bc420
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-07-20 20:58:12 +0530

    Stack using queue

--- Intro/Balanced_Parenthesis.cpp
+++ Intro/Balanced_Parenthesis.cpp
@@ -2,44 +2,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+//         for (int i = 0; i < s.size(); i++)
+//         {
+//             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+//                 st.push(s[i]);
+//             } else {
+//                 switch (s[i])
+//                 {
+//                 case ')':
+//                     if(st.top() == '(') 
+//                     st.pop();
+//                     else 
+//                     return false;
+//                     break;
+//                 case ']':
+//                     if(st.top() == '[') 
+//                     st.pop();
+//                     else 
+//                     return false;
+//                     break;
+//                 case '}':
+//                     if(st.top() == '{') 
+//                     st.pop();
+//                     else 
+//                     return false;
+//                 break;
+//                 default:
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         if(!st.empty()) return false;
+
+//         return true;
+//     }
+// };
+
 class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> st;
+    public:
+        bool isValid(string s){
+             stack<char> st;
         for (int i = 0; i < s.size(); i++)
         {
             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 st.push(s[i]);
             } else {
-                switch (s[i])
-                {
-                case ')':
-                    if(st.top() == '(') 
+                if(st.empty()) return false;
+                char ch = st.top();
+                if(s[i] == ')' && ch == '('
+                || s[i] == ']' && ch == '['
+                || s[i] == '}' && ch == '{'){
                     st.pop();
-                    else 
+                } else {
                     return false;
-                    break;
-                case ']':
-                    if(st.top() == '[') 
-                    st.pop();
-                    else 
-                    return false;
-                    break;
-                case '}':
-                    if(st.top() == '{') 
-                    st.pop();
-                    else 
-                    return false;
-                break;
-                default:
-                    break;
-                }
+                };
             }
         }
-        
-        if(!st.empty()) return false;
-
-        return true;
+        return st.empty();
     }
 };
 
