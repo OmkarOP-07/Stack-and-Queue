568a0ff55c08d3caed4a469350db953299808a02
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-07-21 21:47:47 +0530

    Infix to Postfix

--- Medium_Level/Infix_to_Postfix.cpp
+++ Medium_Level/Infix_to_Postfix.cpp
@@ -1,59 +1,72 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    int priority(char ch)
+    {
+        if (ch == '^')
+            return 3;
+        else if (ch == '*' || ch == '/')
+            return 2;
+        else if (ch == '+' || ch == '-')
+            return 1;
+        else
+            return -1;
+    }
 
-
-    class Solution {
-    public:
-        int priority(char ch){
-            if(ch == '^') return 3;
-            else if(ch == '*' || ch == '/') return 2;
-            else if(ch == '+' || ch == '-') return 1;
-            else return -1;
-        }
-
-        string Infix_To_Postfix(string s) {
-            int i = 0;
-            stack<char> st;
-            string ans;
-            while (i < s.size())   
-            {
-                if(s[i] >= 'A' && s[i] <= 'Z' ||
+    string Infix_To_Postfix(string s)
+    {
+        int i = 0;
+        stack<char> st;
+        string ans;
+        while (i < s.size())
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z' ||
                 s[i] >= 'a' && s[i] <= 'z' ||
-                s[i] >= '0' && s[i] <= '9'){
-                    ans += s[i];
-                } else if (s[i] == '(')
+                s[i] >= '0' && s[i] <= '9')
+            {
+                ans += s[i];
+            }
+            else if (s[i] == '(')
+            {
+                st.push(s[i]);
+            }
+            else if (s[i] == ')')
+            {
+                while (!st.empty() && st.top() != '(')
                 {
-                    st.push(s[i]);
-                } else if (s[i] == ')'){
-                    while (!st.empty() && st.top() != '(')
-                    {
-                        ans += st.top();
-                        st.pop();
-                    }
-                    if (!st.empty()) st.pop();
+                    ans += st.top();
+                    st.pop();
                 }
-                else {
-                    while(!st.empty() && priority(s[i]) <= priority(st.top())){
-                        ans += st.top();    
-                        st.pop();
-                    }
-                    st.push(s[i]);
+                if (!st.empty())
+                    st.pop();
+            }
+            else
+            {
+                while (!st.empty() && priority(s[i]) <= priority(st.top()))
+                {
+                    ans += st.top();
+                    st.pop();
                 }
-                i++;
+                st.push(s[i]);
             }
-            while(!st.empty()){
-                ans += st.top();
-                st.pop();
-            }
-            return ans;
+            i++;
         }
-    };
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
 
 int main()
 {
     Solution sh;
-    cout<<sh.Infix_To_Postfix("a+b*(c^d-e)^(f+g*h)-i");
- return 0;
+    cout << sh.Infix_To_Postfix("a+b*(c^d-e)^(f+g*h)-i");
+    return 0;
 }
