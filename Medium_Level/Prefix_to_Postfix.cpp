2c85c92cd48dd71605eb4076c783690448f7b68e
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-08-05 17:30:54 +0530

    Prefix to Postfix  done

---
+++ Medium_Level/Prefix_to_Postfix.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+string Prefix_to_Postfix(string s){
+    int n = s.size()-1;
+    stack<string> st;
+    while(n >= 0){
+        if(s[n] >= 'A' && s[n] <= 'Z'){
+            st.push(string(1,s[n]));
+        }
+        else {
+            string t1 = st.top(); st.pop();
+            string t2 = st.top(); st.pop();
+            string temp = t1 + t2 + s[n];
+            st.push(temp);
+        }
+        n--;
+    }
+    return st.top();
+}
+
+int main()
+{
+    string s = "*+AB-CD";
+    cout<<"Postfix expression :"<<Prefix_to_Postfix(s);
+ return 0;
+}
