5ed6a9f2028eb960e76e7deebe651bd529e0861d
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-08-13 22:22:58 +0530

    testing some codes

--- Monotonic/test.cpp
+++ Monotonic/test.cpp
@@ -3,30 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> PGE(vector<int> &arr)
+
+vector<int> PGEE(vector<int> &arr)
 {
-    vector<int> ans(arr.size(), -1);
     stack<int> st;
+    vector<int> ans(arr.size(), -1);
     for (int i = 0; i <= arr.size() - 1; i++)
     {
-        while (!st.empty() && st.top() <= 0)
+        while (!st.empty() && arr[st.top()] < arr[i])
+        {
             st.pop();
-        ans[i] = st.empty() ? 0 : st.top();
-        
-        st.push(arr[i]);
+        }
+        ans[i] = st.empty() ? -1 : st.top();
+        st.push(i);
     }
     return ans;
 }
-
 int main()
 {
-    vector<int> arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-    vector<int> PGE_Arr = PGE(arr);
-    for (auto it : arr)
-    {
-        cout << it << " ";
-    }
-    cout<<endl;
+    vector<int> arr = {3, 2, 1, 5, 8, 2, 9};
+    vector<int> PGE_Arr = PGEE(arr);
     for (auto it : PGE_Arr)
     {
         cout << it << " ";
