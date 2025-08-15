f8fc90f8d3c1e199760309fb45d51439f95e87e6
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-08-15 19:37:13 +0530

    done

--- Monotonic/Largest_Rectangle_in_Histogram.cpp
+++ Monotonic/Largest_Rectangle_in_Histogram.cpp
@@ -1,49 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
-    vector<int> NSE(vector<int> &arr) {
-        stack<int> st;
-        vector<int> ans(arr.size(), arr.size());
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-            ans[i] = st.empty() ? arr.size() : st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> PSE(vector<int> &arr) {
-        stack<int> st;
-        vector<int> ans(arr.size(), -1);
-        for (int i = 0; i < arr.size(); i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-            ans[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-
     int largestRectangleArea(vector<int>& arr) {
-        if (arr.size() == 1) return arr[0];
-        
-        vector<int> NSE_arr = NSE(arr);
-        vector<int> PSE_arr = PSE(arr);
+        stack<int> st;
+        int maxArea = 0;
 
-        int greatest = 0;
         for (int i = 0; i < arr.size(); i++) {
-            int width = NSE_arr[i] - PSE_arr[i] - 1;
-            int area = arr[i] * width;
-            greatest = max(greatest, area);
+            
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                int el = st.top(); st.pop();
+                int nse = i;
+                int pse = st.empty()?-1: st.top();
+                maxArea = max(arr[el]*(nse-pse-1), maxArea);
+            }
+            st.push(i);
         }
-        return greatest;
+        return maxArea;
     }
 };
+
 
 int main() {
     Solution sh;
