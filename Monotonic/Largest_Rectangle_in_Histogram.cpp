9d0e975cc01e6c525f1b87684316c8a596cda5ec
Onkar Mallinath Potdar <141763167+OmkarOP-07@users.noreply.github.com>
2025-08-15 20:36:00 +0530

    a

--- Monotonic/Largest_Rectangle_in_Histogram.cpp
+++ Monotonic/Largest_Rectangle_in_Histogram.cpp
@@ -1,22 +1,31 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& arr) {
         stack<int> st;
-        int maxArea = 0;
-
-        for (int i = 0; i < arr.size(); i++) {
-            
-            while (!st.empty() && arr[st.top()] > arr[i]) {
-                int el = st.top(); st.pop();
-                int nse = i;
-                int pse = st.empty()?-1: st.top();
-                maxArea = max(arr[el]*(nse-pse-1), maxArea);
+        int n=arr.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[i]<arr[st.top()]){
+                int el=st.top();
+                st.pop();
+                int left=st.empty()?-1:st.top();
+                int right=i;
+                ans=max(ans,arr[el]*(right-left-1));
             }
             st.push(i);
         }
-        return maxArea;
+         while(!st.empty()){
+                int el=st.top();
+                st.pop();
+                int left=st.empty()?-1:st.top();
+                int right=n;
+                ans=max(ans,arr[el]*(right-left-1));
+            }
+        return ans;
     }
 };
 
